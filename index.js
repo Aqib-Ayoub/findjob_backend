b85a69208c8b687b31ca782f564f646563161470
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-06-28 11:49:34 +0530

    feat: update jobController to fix req.query parsing and improve getAllJobs logic

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const jobRouter = require("./routes/job");
+const bodyParser = require("body-parser");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI).then(()=> console.log('Connected to atlas')).catch(err=> console.log(err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to atlas"))
+  .catch((err) => console.log(err));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/api/jobs", jobRouter);
 
-app.get('/', (req, res)=> res.send('Hello World!'));
+// app.get('/', (req, res)=> res.send('Hello World!'));
 
-app.listen(process.env.PORT || port, ()=> console.log(`Server is running on port ${process.env.PORT}`));
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
