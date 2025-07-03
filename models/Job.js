07b2dc50329071f79a88db2a02bc3fe12edd2210
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-07-03 17:37:36 +0530

    added new features to job.js for improved job handling

--- models/Job.js
+++ models/Job.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const JobSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    location: {type: String, required: true},
-    description: {type: String, required: true},
-    agentName: {type: String, required: true},
-    salery: {type: String, required: true},
-    period: {type: String, required: true},
-    contract: {type: String, required: true},
-    hiring:{type: Boolean,default:true},
-    requirements:{
-        type:Array,
-        required: true,
-
+const JobSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    agentName: { type: String, required: true },
+    salery: { type: String, required: true },
+    period: { type: String, required: true },
+    contract: { type: String, required: true },
+    hiring: { type: Boolean, default: true },
+    requirements: {
+      type: Array,
+      required: true,
     },
-    imageUrl: {type:String, required: true},
-    agentId: {type: String, required: true},
+    imageUrl: { type: String, required: true },
+    agentId: { type: String, required: true },
+    company: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps:true});
-
-module.exports = mongoose.model('Job', JobSchema)
+module.exports = mongoose.model("Job", JobSchema);
