8cd52dd9e40aa16f268134ade2ffc0b4fee49057
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-07-03 17:59:33 +0530

    Add user model to handle user-related data and schema

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    passwword: { type: String, required: true },
+    location: { type: String, required: false },
+    phone: { type: String, required: false },
+    updated: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
+    isAgent: { type: Boolean, default: false },
+    skills: { type: Boolean, default: false },
+    profile: {
+      type: String,
+      required: true,
+      default:
+        "https://avatars.githubusercontent.com/u/52028089?s=400&u=34c2e3cca6ed2b7927934f973d364a51116d4cfa&v=4",
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", UserSchema);
