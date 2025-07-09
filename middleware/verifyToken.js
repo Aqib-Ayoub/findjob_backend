f48788525c16b11ed74513f95f8021925138e4e6
Aqib-Ayoub <aqibayoub321@gmail.com>
2025-07-09 21:32:24 +0530

    added some middleware

---
+++ middleware/verifyToken.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeder = req.header.authorization;
+
+  if (authHeder) {
+    const token = authHeder.spilt(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET, async (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+      req.user = user;
+      next();
+    });
+  }
+};
+const verifyAndAuth = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id || req.user.isAdmin) {
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to access" });
+    }
+  });
+};
+const verifyAgent = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAgent || req.user.isAdmin) {
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to access" });
+    }
+  });
+};
+module.exports = { verifyToken, verifyAgent, verifyAndAuth };
